168605269538267f95b80b8bb26807ceaca7b879
gegecs <145285671+gegecs@users.noreply.github.com>
2024-04-23 14:52:03 -0300

    Add files via upload

---
+++ tabuada1a10.js
@@ -0,0 +1,10 @@
+function tabuada(numeroTab){
+    let numeroMulti = 1;
+    while(numeroMulti <= 10){
+        let resultadoTab = numeroTab * numeroMulti;
+        console.log(resultadoTab);
+        numeroMulti++;
+    }
+}
+
+tabuada(x);
